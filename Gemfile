36ad6878d2f57c194ed35a1c99c105128cade4a3
jkaeser <jakaeser44@gmail.com>
2016-10-11 13:26:41 -0400

    Move style sheets and remove unnecessary files.
    We don't need all the css and js from the parent site.

--- Gemfile
+++ Gemfile
@@ -2,21 +2,16 @@ source "https://rubygems.org"
 ruby RUBY_VERSION
 
 gem "jekyll", "3.3.0"
-# gem "minima", "~> 2.0"
 
-# If you have any plugins, put them here!
 group :jekyll_plugins do
    gem 'jekyll-feed', '~> 0.6'
-  #  gem 'jekyll-assets', '2.2.8'
    gem 'bundler', '1.13.3'
    gem 'rack-jekyll', '0.5.0'
    gem 'rake-jekyll', '1.1.0'
-  #  gem 'github-pages', '98'
    gem 'kramdown', '1.12.0'
    gem 'sass', '>=3.4'
    gem 'uglifier', '3.0.2'
    gem 'breakpoint', '2.7.0'
-  #  gem 'jeet', '6.1.3'
    gem 'compass', '>1.0'
    gem 'respond-to', '2.6'
    gem 'autoprefixer-rails', '6.5.0.2'
---
+++ javascripts/probo.js
@@ -0,0 +1,71 @@
+(function (window, $) {
+
+  // use tinynav
+  $("#sidebar-first .sidebar-nav").tinyNav();
+
+  if ($.fn.sidr instanceof Function) {
+    $('#simple-menu').css('display', '').sidr({side: 'right'});
+  }
+
+  //fallback for css animation breaking on ios scroll
+  var hello = ['Hello', 'Hola', 'Bonjour', 'Bon dia', 'Namaste'];
+  var count = 1;
+  setInterval(function () {
+    $("span.hello").fadeOut(600, function () {
+      $(this).html(hello[count]);
+      count++;
+      if (count == hello.length)
+        count = 0;
+      $(this).fadeIn(500, function () {
+      });
+    });
+  }, 4000);
+
+  $(window).on("resize", function (event) {
+    if ($('body').hasClass('sidr-open') && $(window).width() >= 768) {
+      $.sidr('close');
+    }
+  });
+
+  // Setting up FitVids for responsive iFrames embeds (videos)
+  $('.video-container').fitVids();
+
+  $('#testimonials-carousel').cycle({
+    speed: 600,
+    manualSpeed: 100,
+    fx: 'fade',
+    slides: '> div',
+    swipe: true,
+    pager: '.cycle-pager'
+  });
+
+  //search
+  $('.search a').click(function (e) {
+    $('.search-section').toggleClass('on');
+    $('.site').addClass('overflow-hidden');
+  });
+  $('.search-section .fa-times').click(function (e) {
+    $('.search-section').toggleClass('on');
+    $('.site').removeClass('overflow-hidden');
+  });
+  $('.search-mobile a').click(function (e) {
+    $.sidr('close');
+    $('.site').addClass('overflow-hidden');
+  });
+
+  $(function () {
+    $('#search-query').lunrSearch({
+      indexUrl: '/assets/index.json',   // Url for the .json file containing search index data
+      results: '#search-results',  // selector for containing search results element
+      entries: '.entries',         // selector for search entries containing element (contained within results above)
+      titleMsg  : '<h1>Search Results<h1>',   // message attached in front of results
+      template: '#search-results-template',  // selector for Mustache.js template
+      emptyMsg  : '<p>Nothing found.</p>'     // shown message if search returns no results
+    });
+  });
+
+  $('#search-query').submit(function(e) {
+    e.preventDefault();
+  });
+
+});
