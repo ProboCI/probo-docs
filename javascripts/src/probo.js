3dd26617541baeaa27e740b195cd68e6dbf87064
John Kaeser <jakaeser44@gmail.com>
2017-06-19 17:03:11 +0000

    Writing function documentation.

--- javascripts/src/probo.js
+++ javascripts/src/probo.js
@@ -55,6 +55,10 @@
       };
     };
 
+    /**
+     * Populates search fields with parameters in URL.
+     * @param {Object} urlParams - A URLSearchParams object.
+     */
     function reapplySettings(urlParams) {
       $inputField.val(getQuery(urlParams));
       filters = getFilters(urlParams);
@@ -63,27 +67,44 @@
       }
     }
 
+    /**
+     * Clears the search results area.
+     */
     function clearSearchResults() {
       $('.search__result').remove();
       $('.search__results-count').remove();
     };
 
+    /**
+     * Sets a message in the results area. Intended for use when a search does
+     * not return results.
+     * @param {String} message - The message to print.
+     */
     function setResultsMessage(message) {
       $resultsArea.append('<p class="search__results-count">' + message + '</p>');
     }
 
+    /**
+     * Gets the user-submitted query string from the URL.
+     * @param {Object} urlParams - A URLSearchParams object.
+     * @return {String} The user-submitted query string.
+     */
     function getQuery(urlParams) {
       var query = urlParams.get('query');
       return query;
     }
 
+    /**
+     * Gets the user-selected filters from the URL.
+     * @param {Object} urlParams - A URLSearchParams object.
+     * @return {Object} An object full of filter category keys with their values.
+     */
     function getFilters(urlParams) {
       // Loop through URL parameters Iterator to get filter values.
       // gulp-uglify does not support for...of loops.
       var params = urlParams.entries();
       var nextParam = params.next().value;
       var filters = {};
-
       do {
         if (nextParam[0] != 'query') {
           filters[nextParam[0]] = nextParam[1];
@@ -94,17 +115,24 @@
       return filters;
     };
 
+    /**
+     * Converts a filter object into an array.
+     * @param {Object} filters - Filters returned from the getFilters() function.
+     * @return {Array} An array of strings, formatted as 'key:value'.
+     */
     function filtersToArray(filters) {
       var filterArray = [];
-
       for (key in filters) {
         var value = filters[key];
         filterArray.push(key + ':' + value)
       }
-
       return filterArray;
     }
 
+    /**
+     * Gets search results from the index and prints them to the results area.
+     * @param {Object} urlParams - A URLSearchParams object.
+     */
     function getSearchResults(urlParams) {
       var query = getQuery(urlParams);
       var filters = filtersToArray(getFilters(urlParams));
@@ -112,7 +140,6 @@
         query: query,
         facetFilters: filters
       };
-
       // Ensure we have a real query since empty queries match all in the index
       if (query != '') {
         index.search(searchObj,
@@ -121,11 +148,9 @@
             console.error(err);
             return;
           }
-
           var resultsLabel = content.hits.length == 1 ? ' result' : ' results';
           var resultsMessage = 'Showing ' + content.hits.length + resultsLabel + ' for "' + query + '"';
           $('.page-title').replaceWith('<h1 class="page-title">' + resultsMessage + '</h1>');
-
           var results = [];
           for (var h in content.hits) {
             var hit = content.hits[h];
